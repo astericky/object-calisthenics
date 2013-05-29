444843d65a653342a6111d4f985f30e73f8f7154
Chris Sanders <astericky@gmail.com>
2013-05-29 13:27:58 -0400

    update tests and SpecRunner.html

--- spec/JobApplicationSpec.js
+++ spec/JobApplicationSpec.js
@@ -1,57 +1,23 @@
-describe('a Recruiter', function() {
-    var recruiterOutput;
-    var recruiterJobsOutput;
-    var testRecruiter = 'ID: 1 / Name: Chris';
-    var testRecruiterPostJob = 'Web Developer';
-    var recruiterJobs = new RecruiterJobs();
-    var recruiter = new Recruiter(new Name('Chris'), new Id(1));
-    var aDisplay = new Display();
-
-    recruiterJobs.post('Web Developer', recruiter);
-    recruiterOutput = recruiter.displayOn(aDisplay);
-    recruiterJobsOutput = recruiterJobs.postedBy(recruiter)[0].represent(aDisplay);
-    console.log(recruiterJobsOutput);
-    
-    it('exists', function() {
-        expect(typeof recruiter).toBe('object');
-    });
-
-    it('has a name and id', function() {
-        expect(recruiterOutput).toBe(testRecruiter);
-    });
-
-    it('can post jobs', function() {
-        expect(recruiterJobsOutput).toBe(testRecruiterPostJob);
+describe('Job Board', function() {
+    beforeEach(function() {
+        var jobseeker = new Jobseeker(new Name('Chris'), new Id(32));
+        var recruiter = new Recruiter(new Name('Sean'), new Id(1));
+        var jobs = new RecruiterJobs();
+        var aDisplay = new Display();
     });
     
-});
-
-
-describe('a Jobseeker', function() {
-    it('exists', function() {
+    describe('Jobseeker', function() {
+        it('has current jobseeker name and id', function() {
+            expect(jobseeker).toBeDefined();
+            expect(jobseeker.displayOn(aDisplay)).toEqual('ID: 32 / Name: Chris');
+        });
     });
 
-    it('has a name and id', function() {
+    describe('Recruiter', function() {
+        it('has current recruiter name and id', function() {
+            expect(recruiter).toBeDefined();
+            expect(recruiter.displayOn(aDisplay)).toEqual('ID: 32 / Name: Chris');
+        });
     });
-
-    it('can save jobs', function() {
-    });
-
-    it('can apply to ATS jobs posted by recruiters', function() {
-    });
-    
-    it('can apply to JReq jobs if they have a resume', function() {
-    });
-
-    it('cannot apply to JReq jobs with someone else\'s resume', function() {
-    });
-
-    it('can have more than one resume', function() {
-    });
-    
-    it('can see a listing of jobs for which they applied', function() {
-    });
-
-
 });
 
