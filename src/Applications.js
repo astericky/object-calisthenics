2a9fa0402f815773b5c5674e48a9aaf892f90d71
Chris Sanders <astericky@gmail.com>
2013-06-04 12:46:35 -0400

    added Applications object

---
+++ src/Applications.js
@@ -0,0 +1,45 @@
+function Applications(applications) {
+
+    function equals(otherJobs) {
+        return isSameSizeAs(otherJobs) && isSubsetOf(otherJobs); 
+    }
+
+    function size() {
+        return jobs.length;
+    }
+
+    function isSameSizeAs(otherJobs) {
+        return size() === otherJobs.size();
+    }
+
+    function exists(job) {
+        return selectByJob(job);
+    }
+
+    function isSubsetOf(otherJobs) {
+        return any(otherJobs.exists);
+    }
+
+    function selectByJob(job) {
+        return function(otherJob) {
+            return job.equals(otherJob);
+        };
+    }
+
+    function any(fn) { 
+        var length = size();
+        for (var i = 0; i < length; i++) {
+            if (!fn(jobs[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return {
+        equals : equals,
+        exists : exists,
+        isSameSizeAs : isSameSizeAs,
+        size : size,
+    };
+}
