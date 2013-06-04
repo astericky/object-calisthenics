484580e998ddb2c35d057526d0e9d031b56f5338
Chris Sanders <astericky@gmail.com>
2013-06-04 12:37:18 -0400

    added SubmittedJobApplication object

---
+++ src/SubmittedJobApplication.js
@@ -0,0 +1,15 @@
+function SubmittedJobApplication(jobApplication, date) {
+   
+    function equals(submittedJobApplication) {
+    }
+
+    function getJobApplication() {
+       return jobApplication;
+    }
+
+    return {
+        equals : equals,
+        getJobApplication : getJobApplication,
+        getDate : getDate
+    };
+}
